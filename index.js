d236f76fc124a6709b1e1cf9974db70b3dac9b34
Henrique Lopes Ferreira <henrique@unihospitalar.com>
2023-07-05 17:51:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const port = process.env.PORT;
+
+const URL = process.env.URL_DB
+mongoose
+    .connect(URL)
+    .then(console.log("Banco de dados conectado com sucesso"))
+    .catch((error) => {
+        console.log(error);
+    });
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+// band = {
+// 	nome:
+// 	cnpj:
+// 	integrantes:[]
+// 	Genêro:[]
+// 	endereco{
+// 		estado:
+// 		cidade:
+// 		bairro:
+// 	}
+// }
+
+// artista = {
+// 	nome:
+// 	cpf:
+// 	dataNascimento:
+// 	desrição:
+// 	instrumentos:[]
+// 	estado:
+// 	cidade:
+// 	bairro:
+// 	numero:
+// 	complemento:
+// 	observação:
+// }
+
+// Genêro = {
+// 	nome:
+// 	decricão:
+// }
+
+// instrumentos:{
+// 	nome:
+// 	descrição
+// }
